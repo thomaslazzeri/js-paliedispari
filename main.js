3e464bdbaf59001aa75caa833864c7831cb77d91
thomaslazzeri3 <thomaslazzeri3@gmail.com>
2026-05-17 17:53:42 +0200

    add if for check cancel of user and add variable spaces and place lowercase

--- main.js
+++ main.js
@@ -13,10 +13,18 @@
 const utenteParola = prompt("Inserisci una parola");
 //Creiamo una funzione per capire se la parola inserita è palindroma
 function palindroma(parola) {
+    // Controlla se l'utente ha premuto 'Annulla'
+    if (parola === null) {
+        return;
+    }
+    //Rimuove gli spazi e converte tutto in minuscolo
+    //Il regex /\s+/g trova tutti gli spazi e li sostituisce con stringhe vuote
+    let parolaPulita = parola.toLowerCase().replace(/\s+/g, '');
+
     //Inverto la parola
-    const parolaInvertita = parola.split("").reverse().join("");
+    const parolaInvertita = parolaPulita.split("").reverse().join("");
     //Confronto la parola originale con quella invertita e se sono uguali, la parola è palindroma
-    if (parola === parolaInvertita) {
+    if (parolaPulita === parolaInvertita) {
         return "La parola è palindroma";
     }
     //Se non sono uguali, la parola non è palindroma
